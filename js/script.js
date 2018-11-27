5403f744adcc0e4b32e9d4130c41c471d7af09e4
Shandon Mith <malcolm721@gmail.com>
2018-11-27 22:00:17 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,50 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+   var person;
+  function Name() {
+   person=window.prompt("Please enter your name");
 
-//GLOBAL VARIABLES
-/* global $ */
+    }
+var inputVal;
+var computer=["rock","paper","scissors"];
+function game(){
+    
+    var number=Math.floor(Math.random() * 3);
+    $('#userChoice').text(inputVal);
+    $('#computerChoice').text(computer[number]);
+    
+    if(inputVal === computer[number]){
+        $('#result').text("It is a tie");
+        
+    
+    } else if((inputVal === 'rock' && computer[number] === 'scissors') || 
+    (inputVal === 'paper' && computer[number] === 'rock') || 
+    (inputVal === 'scissors' && computer[number] === 'paper')) {
+        $('#result').text(person + ' Wins');
+    }else if((inputVal === 'rock' && computer[number] === 'paper') || 
+    (inputVal === 'paper' && computer[number] === 'scissors') || 
+    (inputVal === 'scissors' && computer[number] === 'rock')){
+        $('#result').text('Computer Wins');
+    }
+}
 
+$('#shoot').click(function() {
+    inputVal = $("#input").val();
+    if(inputVal === ''){
+        alert("You didn't input anything");
+    }else if(inputVal != 'rock' && inputVal != 'paper' && inputVal != 'scissors'){
+        alert("Not a valid input");
+        
+        
+    }else{
+        game();
+    }
 
+    
 
-// DOCUMENT READY FUNCTION BELOW
+    
+    
+    
+
+    
+
+});
 
